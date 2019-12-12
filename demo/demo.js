4dca7696e9bc8a6207699dce4b4ea326909723d4
ninabreznik <ninabreznik@gmail.com>
2019-12-13 06:43:32 +0700

    Crashing stream, but somehow working

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,9 @@
 const contractsDB = require('contracts-db')
 const smartcontractcodes = require('../')
 
-//const daturl = 'dat://ee970fe30a2b564475eb0468acf3de9363fb6b6ef775de26fc3be90ec7dbed72'
-const daturl = 'dat://786130fdd86da6cd579669b6075049cb589f12cbe4c8bf5d9fe350c1b677c5d6'
+const dat = '48a050618e16da68395cbe8fd7b3c0b0df667dab1fcdb1b0f1094f5bf6466ca4'
 const cardsCount = 8
-const db = contractsDB(daturl, cardsCount)
+const db = contractsDB(dat, cardsCount)
 
 const element = smartcontractcodes({
   contracts: db,
